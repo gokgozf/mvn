cc52a63c328cc42db8450d3db9b25033b375837f
unknown <fahrettin.goekgoez@ada.lorien.fkie.fgan.de>
2015-09-04 10:26:31 +0200

    project demo

---
+++ src/main/java/goekgoez/com/mvngithubdemo/App.java
@@ -0,0 +1,13 @@
+package goekgoez.com.mvngithubdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
